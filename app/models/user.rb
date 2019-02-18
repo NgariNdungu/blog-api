bc356b41a1ec07428641f792a86348646784597b
Ngari Ndung'u <ngari.ndungu@gmail.com>
2019-02-18 12:39:42 +0300

    add validations on user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
   has_secure_password
+  validates :username, :full_name, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_15_144005) do
+ActiveRecord::Schema.define(version: 2019_02_18_091538) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string "username", limit: 20
+    t.string "username", limit: 20, null: false
     t.string "password_digest"
     t.string "full_name"
     t.datetime "created_at", null: false
