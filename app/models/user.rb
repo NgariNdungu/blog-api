e64efbbc9c584258bb659d3401bbf726a687ee4e
Ngari Ndung'u <ngari.ndungu@gmail.com>
2019-02-19 16:06:05 +0300

    update associations and validations on user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
+  has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
   validates :username, :full_name, presence: true
+  validates :username, uniqueness: true
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -12,4 +12,19 @@ class UserTest < ActiveSupport::TestCase
     assert_not build(:user, full_name: "").save, "Saved a user without a name"
     assert_not build(:user, password: "").save, "Saved a user without a password"
   end
+
+  test 'user should have posts' do
+    assert_respond_to create(:user), :posts, "User doesn't have posts"
+  end
+
+  test 'user should have comments' do
+    assert_respond_to create(:user), :comments, "User can't have comments"
+  end
+
+  test 'should not save user with duplicate username' do
+    create(:user)
+    assert_raises(ActiveRecord::RecordInvalid) do
+      create(:user)
+    end
+  end
 end
