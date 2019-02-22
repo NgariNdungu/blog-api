4b1305560ca47bc770b6857c2748bf07ba516e44
Ngari Ndung'u <ngari.ndungu@gmail.com>
2019-02-22 12:43:54 +0300

    set default json adapter

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)    
     if @user.save
-      render json: @user, serializer: UserSerializer, adapter: :json_api
+      render json: @user
     else
       render json: {"errors": @user.errors.messages.to_json}, status: :bad_request
     end
